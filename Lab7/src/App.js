d0e956a0c1af306b9f1f12dcdd7ba1e0bc5a4f75
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-12-19 16:48:44 +0200

    completed the first task

---
+++ Lab7/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './task1/Header';
+import Content from './task1/Content';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
