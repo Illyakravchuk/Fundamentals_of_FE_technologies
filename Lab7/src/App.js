de05249222f55c38cfa663dc7b45596eb179040d
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-12-19 18:36:03 +0200

    completed the second task

--- Lab7/src/App.js
+++ Lab7/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './task1/Header';
 import Content from './task1/Content';
-
+import Footer  from "./task2/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Footer  />
     </div>
   );
 }
